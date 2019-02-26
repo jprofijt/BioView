ea6f7ac075078086774672d16b62444a1687104c
jprofijt <jouke.profijt@protonmail.com>
2019-02-26 13:57:00 +0100

    Intitial commit

---
+++ src/main/java/nl/bioinf/jp_kcd_wr/image_library/ImageLibraryApplication.java
@@ -0,0 +1,13 @@
+package nl.bioinf.jp_kcd_wr.image_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/bioinf/jp_kcd_wr/image_library/ImageLibraryApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.bioinf.jp_kcd_wr.image_library;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImageLibraryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
