5c1f3efa08a181624250aa2d23b0e1ec106e919e
whreitsma <w.h.reitsma@st.hanze.nl>
2019-02-27 11:58:37 +0100

    Edited data.sql Created Logincontroller.java, WebSecurityConfig.java

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (name, email, password, role)
 VALUES
-       ("Tom", "tom.de.vries@hanze.nl", "iets123", "user"),
-       ("Lisa","Lisa67@smoething.nl","bLAbla","user"),
-       ("piet", "piet@hanze.nl", "dinges", "admin");
+       ("Tom", "tom.de.vries@hanze.nl", "iets123", "USER"),
+       ("Lisa","Lisa67@smoething.nl","bLAbla","USER"),
+       ("piet", "piet@hanze.nl", "dinges", "ADMIN");
 
