4ca716a0593372ddf79025226c5e506e022173f4
jprofijt <jouke.profijt@protonmail.com>
2019-02-27 18:33:44 +0100

    Created directory indexer

--- src/main/java/nl/bioinf/jp_kcd_wr/image_library/imagebrowser/FolderSearcher.java
+++ src/main/java/nl/bioinf/jp_kcd_wr/image_library/imagebrowser/FolderSearcher.java
@@ -1,4 +1,15 @@
 package nl.bioinf.jp_kcd_wr.image_library.imagebrowser;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class FolderSearcher {
+    public ArrayList<String> findFolders(String nextFolders){
+        File[] directories = new File(nextFolders).listFiles(File::isDirectory);
+        ArrayList<String> folders = new ArrayList<>();
+        for (File folder: directories) {
+            folders.add(folder.getPath().replace(nextFolders + "/", ""));
+        }
+        return folders;
+    }
 }
