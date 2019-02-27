9326be5b3035eda889bac26be3e1bf506e817fc9
jprofijt <jouke.profijt@protonmail.com>
2019-02-27 18:35:03 +0100

    Added Basic folder indexing controller

--- src/main/java/nl/bioinf/jp_kcd_wr/image_library/imagebrowser/FolderStructureController.java
+++ src/main/java/nl/bioinf/jp_kcd_wr/image_library/imagebrowser/FolderStructureController.java
@@ -1,4 +1,23 @@
 package nl.bioinf.jp_kcd_wr.image_library.imagebrowser;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.File;
+import java.util.ArrayList;
+
+@Controller
 public class FolderStructureController {
+    @GetMapping("/nextfolder")
+    public String NextFolder(@RequestParam(name="folder", required=false, defaultValue="testdata") String folder, Model model) {
+        FolderSearcher folderFinder = new FolderSearcher();
+        model.addAttribute("folders", folderFinder.findFolders(folder));
+        return "folders";
+    }
+
+
+
 }
+
