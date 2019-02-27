0382785b8e62df56435f3a3faaf5b42a89791aeb
jprofijt <jouke.profijt@protonmail.com>
2019-02-27 18:28:26 +0100

    Started work a directory browser

---
+++ src/test/java/nl/bioinf/jp_kcd_wr/image_library/imagebrowser/FolderSearcherTest.java
@@ -0,0 +1,12 @@
+package nl.bioinf.jp_kcd_wr.image_library.imagebrowser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FolderSearcherTest {
+
+    @Test
+    void findFolders() {
+    }
+}
