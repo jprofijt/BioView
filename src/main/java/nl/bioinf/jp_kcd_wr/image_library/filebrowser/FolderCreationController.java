e8ecd77b2435e8fd81afa50d847663356955a1c6
Jouke Profijt <jouke.profijt@protonmail.com>
2019-03-05 15:29:16 +0100

    created controller that maps to /createfolder, that creates a folder in the current directory

--- src/main/java/nl/bioinf/jp_kcd_wr/image_library/filebrowser/FolderCreationController.java
+++ src/main/java/nl/bioinf/jp_kcd_wr/image_library/filebrowser/FolderCreationController.java
@@ -1,13 +1,25 @@
-package nl.bioinf.jp_kcd_wr.image_library.imagebrowser;
+package nl.bioinf.jp_kcd_wr.image_library.filebrowser;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+@Controller
 public class FolderCreationController {
-    @PostMapping("/createfolder.do")
-    public
+    @PostMapping("/createfolder")
+    public String CreateFolder(@RequestParam(name="directoryName", required=true) String directoryName, @RequestParam(name="currentPath", required=true) String currentPath, Model model) {
+        FolderHandler creator = new FolderHandler();
+        try {
+            creator.createNewFolder(directoryName, currentPath);
+        } catch (DirectoryExistsException e) {
+            return "directory-error";
+        }
+        model.addAttribute("folders", creator.getNextFolders(currentPath));
+        model.addAttribute("currentPath", currentPath);
+        return "folders";
+    }
 
     }
-}
+
