9d69a7fa2712f2d53a666d7d73fb3a604537b2aa
Jouke Profijt <jouke.profijt@protonmail.com>
2019-03-05 09:51:28 +0100

    adapted to new interface

--- src/main/java/nl/bioinf/jp_kcd_wr/image_library/imagebrowser/FolderSearcher.java
+++ src/main/java/nl/bioinf/jp_kcd_wr/image_library/imagebrowser/FolderSearcher.java
@@ -3,13 +3,17 @@ package nl.bioinf.jp_kcd_wr.image_library.imagebrowser;
 import java.io.File;
 import java.util.ArrayList;
 
-public class FolderSearcher {
-    public ArrayList<String> findFolders(String nextFolders){
+public class FolderSearcher implements FolderStructureProvider {
+
+    @Override
+    public ArrayList<String> getNextFolders(String nextFolders){
         File[] directories = new File(nextFolders).listFiles(File::isDirectory);
         ArrayList<String> folders = new ArrayList<>();
         for (File folder: directories) {
-            folders.add(folder.getPath().replace(nextFolders + "/", ""));
+            folders.add(folder.getPath());
         }
         return folders;
     }
+
+
 }
