3f5fe57c29c1dba29c7a5c35c7250a939c321f25
whreitsma <w.h.reitsma@st.hanze.nl>
2019-03-05 16:05:21 +0100

    created: home.html, succes.html, MvcConfig.java updated webSequretyConfig: it will now go to the correct login page updated Schema.sql

--- src/main/java/nl/bioinf/jp_kcd_wr/image_library/config/WebSecurityConfig.java
+++ src/main/java/nl/bioinf/jp_kcd_wr/image_library/config/WebSecurityConfig.java
@@ -1,30 +1,81 @@
 package nl.bioinf.jp_kcd_wr.image_library.config;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import javax.sql.DataSource;
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private DataSource dataSource;
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
                 .authorizeRequests()
-                .antMatchers("/login").permitAll()
+                .antMatchers("/", "/home", "/login").permitAll()//, "/**/home"
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
                 .permitAll()
+                .defaultSuccessUrl("/uploadForm")
                 .and()
+                /*The mapping /logout will automatically log the user out, without any html page. Will redirect to /login?logout*/
                 .logout()
                 .permitAll();
     }
 
 
 
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("user").password("user").roles("USER");
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withUsername("admin").password("admin").roles("USER","ADMIN").build());
+//        return manager;
+//    }
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    /**
+     * Database security configuration for the login system.
+     *
+     * @param auth            authentication manager
+     * @param passwordEncoder the password encoder
+     * @throws Exception the exception
+     */
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .passwordEncoder(passwordEncoder)
+                .usersByUsernameQuery(
+                        "select name, password from users where email = ?")
+                .authoritiesByUsernameQuery(
+                        "select email, role from users where email = ?");
+    }
 }
--- src/main/resources/Schema.sql
+++ src/main/resources/Schema.sql
@@ -1,11 +1,15 @@
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE users(
-  id        INT(100)     NOT NULL AUTO_INCREMENT,
-  name      VARCHAR(40)  NOT NULL,
-  email     VARCHAR(254) NOT NULL,
-  password  VARCHAR(128) NOT NULL,
-  role      VARCHAR(6)   NOT NULL,
+CREATE TABLE IF NOT EXISTS users(
+  id                 INT(100)     NOT NULL AUTO_INCREMENT,
+  name               VARCHAR(40)  NOT NULL,
+  email              VARCHAR(254) NOT NULL,
+  password           VARCHAR(128) NOT NULL,
+  role               VARCHAR(6)   NOT NULL,
+  enabled            TINYINT,
+  created_date       DATETIME,
+  password_token     VARCHAR(200),
+  password_timestamp DATETIME,
   PRIMARY KEY (id)
 
 );
