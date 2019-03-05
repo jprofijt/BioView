7e1764df624ca33142b313527b899095acdf8eb0
Jouke Profijt <jouke.profijt@protonmail.com>
2019-03-05 15:31:57 +0100

    refactored test

--- src/test/java/nl/bioinf/jp_kcd_wr/image_library/filebrowser/FolderSearcherTest.java
+++ src/test/java/nl/bioinf/jp_kcd_wr/image_library/filebrowser/FolderSearcherTest.java
@@ -1,15 +1,13 @@
-package nl.bioinf.jp_kcd_wr.image_library.imagebrowser;
+package nl.bioinf.jp_kcd_wr.image_library.filebrowser;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class FolderSearcherTest {
 
     @Test
-    void findFolders() {
-        FolderController folderSearcher = new FolderController();
-        String[] folders = folderSearcher.findFolders("testdata").toArray(new String[0]);
+    void getNextFolders() {
+        FolderHandler folderSearcher = new FolderHandler();
+        String[] folders = folderSearcher.getNextFolders("testdata").toArray(new String[0]);
 
         for (String folder: folders) {
             System.out.printf("directory: " + folder + "\n");
