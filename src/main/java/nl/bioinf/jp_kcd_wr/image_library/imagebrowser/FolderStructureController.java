0cfcc4b5c92678924703b8a869959a1ff84699b6
Jouke Profijt <juke@ux360uak.Zen>
2019-03-05 09:49:46 +0100

    adapted to new interface

--- src/main/java/nl/bioinf/jp_kcd_wr/image_library/imagebrowser/FolderStructureController.java
+++ src/main/java/nl/bioinf/jp_kcd_wr/image_library/imagebrowser/FolderStructureController.java
@@ -5,15 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.File;
-import java.util.ArrayList;
-
 @Controller
 public class FolderStructureController {
     @GetMapping("/nextfolder")
     public String NextFolder(@RequestParam(name="folder", required=false, defaultValue="testdata") String folder, Model model) {
         FolderSearcher folderFinder = new FolderSearcher();
-        model.addAttribute("folders", folderFinder.findFolders(folder));
+        model.addAttribute("folders", folderFinder.getNextFolders(folder));
         return "folders";
     }
 
