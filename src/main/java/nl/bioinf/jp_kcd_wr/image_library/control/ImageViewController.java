e26f8943f41b3cd41ace5e0fd6ce932a892eb053
jprofijt <jouke.profijt@protonmail.com>
2019-04-01 20:08:03 +0200

    images can now be viewed from main directory

--- src/main/java/nl/bioinf/jp_kcd_wr/image_library/control/ImageViewController.java
+++ src/main/java/nl/bioinf/jp_kcd_wr/image_library/control/ImageViewController.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @Controller
 public class ImageViewController {
@@ -21,17 +23,11 @@ public class ImageViewController {
     public ImageViewController(StorageService storageService) {this.storageService = storageService;}
 
     @GetMapping("/imageview")
-    public String getImageLocations(Model model) throws IOException {
-        ArrayList<Image> images = new ArrayList<Image>();
-        for(int i = 1 ; i <= 3; i++) {
-            Image image = new Image();
-            image.setId(1);
-            image.setOrigName("Test.png");
-            image.setNewFilename("dummy-200x200.png");
-            image.setPath("images/dummy-200x200.png");
-            images.add(image);
-        }
-        model.addAttribute("images", images);
+    public String getImages(@RequestParam(name="location", required = false, defaultValue = "") String location, Model model) {
+        model.addAttribute("Images", storageService.loadAll(location).map(
+                path -> MvcUriComponentsBuilder.fromMethodName(DirectoryController.class,
+                        "serveFile", path.getFileName().toString()).build().toString())
+                .collect(Collectors.toList()));
 
         return "main-page";
     }
