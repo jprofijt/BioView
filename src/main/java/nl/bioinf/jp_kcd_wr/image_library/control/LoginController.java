2023b3745226898b09fbb7c6991afb3d0d432810
kcduong <k.c.duong@st.hanze.nl>
2019-04-03 22:18:21 +0200

    Small changes to display images in the folder page.

--- src/main/java/nl/bioinf/jp_kcd_wr/image_library/control/LoginController.java
+++ src/main/java/nl/bioinf/jp_kcd_wr/image_library/control/LoginController.java
@@ -3,9 +3,11 @@ package nl.bioinf.jp_kcd_wr.image_library.control;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class LoginController {
     @GetMapping(value={"/login", "/"})
     public ModelAndView loginController(){
