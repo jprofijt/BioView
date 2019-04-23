6b9dfa5755ef2c05048931b0635cdaf80adfec8b
whreitsma <w.h.reitsma@st.hanze.nl>
2019-04-23 12:00:22 +0200

    commit for working at home, updated double click functionality for images.

--- src/main/resources/static/js/imageview.js
+++ src/main/resources/static/js/imageview.js
@@ -1,4 +1,15 @@
-function openimage(){
+// $(document).on("dblclick", )(function(){
+//     $( "#bioimage" ).dblclick(function() {
+//         var path = $("#bioimage").val();
+//         window.location.replace($(this).attr(path));
+//         alert("it works: " + path);
+//     });
+// });
+$(document).on("dblclick", "#bioimage", function openImageOnDblclick(path) {
+    // window.location.replace($(this).attr(path));
+    alert("it works: " + path);
+    //code here
+});
 
-}
+
 
