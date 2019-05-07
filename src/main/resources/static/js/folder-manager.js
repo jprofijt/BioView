05a946ceb8230d2946f7e5b862362b9eef6f0177
kcduong <k.c.duong@st.hanze.nl>
2019-05-07 16:01:22 +0200

    Started with basic folder layout. Still broken.

---
+++ src/main/resources/static/js/folder-manager.js
@@ -0,0 +1,18 @@
+// $(document).ready(function () {
+//     function letGoSmallA(a) {
+//         var getType = $(a).attr("data-file-icon");
+//         if (getType == "folder") {
+//             $(a)
+//                 .children("b")
+//                 .prepend('<i class="fas fa-folder"></i>');
+//         } }
+//     if (newData == "newData") {
+//         $('[data-new="new"][data-file-icon]').each(function() {
+//             letGoSmallA(this);
+//         });
+//     } else {
+//         $("[data-file-icon]").each(function() {
+//             letGoSmallA(this);
+//         });
+//     }
+// }
