1858c593ebc3b6d7e7b0eec61eee1adefeb36ba1
kcduong <k.c.duong@st.hanze.nl>
2019-05-09 15:42:58 +0200

    Selection box now appears when clicking on a folder.

--- src/main/resources/static/js/folder-manager.js
+++ src/main/resources/static/js/folder-manager.js
@@ -1,18 +1,48 @@
-// $(document).ready(function () {
-//     function letGoSmallA(a) {
-//         var getType = $(a).attr("data-file-icon");
-//         if (getType == "folder") {
-//             $(a)
-//                 .children("b")
-//                 .prepend('<i class="fas fa-folder"></i>');
-//         } }
-//     if (newData == "newData") {
-//         $('[data-new="new"][data-file-icon]').each(function() {
-//             letGoSmallA(this);
-//         });
-//     } else {
-//         $("[data-file-icon]").each(function() {
-//             letGoSmallA(this);
-//         });
-//     }
+$(document).ready(function() {
+    $("#folders li").each(function() {
+        var getType = $(this).attr("data-file-icon");
+        if (getType == "folder") {
+            $(this)
+                .children("b")
+                .prepend('<i class="fas fa-folder"></i>');
+        }
+    });
+
+});
+
+$(document).on("click", "[data-file-icon]", function(e) {
+    if (e.ctrlKey) {
+        $(this).addClass("select");
+        // $(this).removeClass("renaming");
+    } else {
+        $(".select").removeClass("select");
+        $(this)
+            .addClass("select")
+            .siblings()
+            .removeClass("select");
+        // $(this).removeClass("renaming");
+    }
+});
+
+
+$(document).on("click dblclick", function() {
+    $("[data-file-icon]")
+        // .removeClass("context-visible")
+        .removeClass("select");
+    // $(".append-option-box").remove();
+    // removeUnwanted();
+    // $(".name").attr("contenteditable", false);
+});
+// $(document).on("click contextmenu", ".append-option-box", function(e) {
+//     e.stopPropagation();
+//     $("[data-file-icon]")
+//         .removeClass("context-visible")
+//         .removeClass("select");
+//     $(".append-option-box").remove();
+// });
+$(document).on("click", "[data-file-icon]", function(e) {
+    e.stopPropagation();
+});
+// function removeUnwanted(){
+//     $('.active-folder-wrapper ~ .active-folder-wrapper,.active-folder-wrapper ~ .no-item-inside-folder').remove();
 // }
