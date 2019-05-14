8a0057156ac36a656f3ff784053f01a9b43f35d5
kcduong <k.c.duong@st.hanze.nl>
2019-05-14 14:11:39 +0200

    Context menu date folder creation now functional.

--- src/main/resources/static/js/folder-manager.js
+++ src/main/resources/static/js/folder-manager.js
@@ -28,6 +28,7 @@ $(document).on("click", "[data-file-icon]", function(e) {
             .addClass("select")
             .siblings()
             .removeClass("select");
+        $('.append-option-box').css("visibility", "hidden");
     }
 });
 
@@ -44,34 +45,7 @@ $(document).on("click", "[data-file-icon]", function(e) {
 
 /*---Context Menu ---*/
 
-// $(document).on("contextmenu", "[data-file-icon]:not(.show-up)", function(e) {
-//     var off = $(this).offset();
-//     var topPos = e.pageY;
-//     var leftPos = e.pageX;
-//     $(".append-option-box").remove();
-//     $(this)
-//         .addClass("context-visible")
-//         .addClass("select");
-//     $(this).append(
-//         '<div class="append-option-box" style="top:' +
-//         topPos +
-//         "px;left:" +
-//         leftPos +
-//         'px;"><div class="inner-contenxt-box"><div data-open="data-move">Open</div><div data-function="data-copy">Copy</div> <div data-function="data-move">Move</div> <div data-function="data-rename">Rename</div> <div data-function="data-delete">Delete</div> <div class="">Share</div> <div data-function="data-properties">Properties</div></div></div>'
-//     );
-//     $(".append-option-box>div>div:has(div)").addClass("has-sub-context");
-//     if ($(this).attr("data-file-icon") != "folder") {
-//         $(".append-option-box .inner-contenxt-box").append(
-//             '<div data-function="data-copy-path">Copy Path</div>'
-//         );
-//         $(".append-option-box .inner-contenxt-box").append(
-//             '<div data-function="data-copy-secure-path">Copy Secure Path</div>'
-//         );
-//     }
-//     return false;
-// });
 $(document).on("contextmenu", ".folder-manager", function(e) {
-    // var off = $(this).offset();
     var top = e.pageY;
     var left = e.pageX;
     // $(".append-option-box").remove();
@@ -83,28 +57,31 @@ $(document).on("contextmenu", ".folder-manager", function(e) {
     //     '    </div>\n' +
     //     '</div>'
     //     );
-    $('.append-option-box').css("visibility", "hidden");
+    // $('.append-option-box').css("visibility", "hidden");
 
     $('.append-option-box').css({"visibility" : "visible", "top": top + "px", "left": left+"px"});
-    // $(".append-option-box>div>div:has(div)").addClass("has-sub-context");
-    // $(".has-sub-context").append('<i class="fas fa-chevron-right"></i>');
     return false;
 });
+
 $(document).on("click contextmenu dblclick", function() {
     $("[data-file-icon]")
-        // .removeClass("context-visible")
         .removeClass("select renaming");
     $('.append-option-box').css("visibility", "hidden");
-    // removeUnwanted();
-    // $(".name").attr("contenteditable", false);
 });
 $(document).on("click contextmenu", ".append-option-box", function(e) {
     e.stopPropagation();
     $("[data-file-icon]")
-        // .removeClass("context-visible")
         .removeClass("select");
     $('.append-option-box').css("visibility", "hidden");
 });
 $(document).on("click contextmenu", "[data-file-icon]", function(e) {
     e.stopPropagation();
 });
+
+function createDateFolder() {
+    $('.date-folder-container').submit();
+}
+
+// function showPopupFolder() {
+//     $('.popup-folder-creation').css("visibility", "visible");
+// }
