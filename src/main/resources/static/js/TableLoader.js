4bf29bf0cf3c4d574bc56e8d80c1f7912b445965
jprofijt <jouke.profijt@protonmail.com>
2019-05-27 15:57:10 +0200

    users can now add new tags to an image from ui

--- src/main/resources/static/js/TableLoader.js
+++ src/main/resources/static/js/TableLoader.js
@@ -7,16 +7,30 @@ $(document).ready(function () {
             new LoadTagTable(id);
             console.log(id);
         });
+
     });
+
+let available;
+const url = "http://"+document.location.hostname + ":8081/api/tags/all/";
+$.getJSON(url, function (result) {
+    available = result;});
+
+
+$(document).ready(function () {
+    $('.newTag').typeahead({
+        highlight: true,
+        source: available
+    })
+});
 
 /**
  * inserts the tags of an image using BioView Api
  * @param id id of image
- * @constructor
+ *
  * @author Jouke Profijt
  */
 function LoadTagTable(id) {
-    const url = "http://"+document.location.hostname + ":8081/api/tags/?image=" + id.toString();
+    const url = "http://"+document.location.hostname + ":8081/api/tags/image/?image=" + id.toString();
     $.ajax({
         url: url,
         type: "GET",
@@ -27,5 +41,36 @@ function LoadTagTable(id) {
             })
         }
     })
+
+}
+
+/**
+ * sends a new tag to the BioView Api
+ * @param element button element
+ * @param user the user that added the new tag
+ * @param id image id
+ *
+ * @author Jouke Profijt
+ */
+function AddTag(element, user, id) {
+    //const url = 'http://'+document.location.hostname + ':8081/api/tags/';
+    const tag = $('#tag-input-' + id).val();
+    const data = {
+        id: id,
+        tag: tag,
+        user: user
+    };
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: data,
+        success: function () {
+            $('#ImageTags-' + id).append("<tr><td>" + $('#tag-input-' + id).val() + "</td></tr>")
+        },
+        dataType: 'application/json'
+    })
+
+
 
 }
