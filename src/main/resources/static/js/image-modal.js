28c0833c86016444ab61b8aabd7e5b354d54ec74
whreitsma <w.h.reitsma@st.hanze.nl>
2019-05-28 16:05:09 +0200

    implementing boundaries for image editing window

--- src/main/resources/static/js/image-modal.js
+++ src/main/resources/static/js/image-modal.js
@@ -20,30 +20,75 @@ function loadImageIntoCanvas(id, path){
             width: image.width
         });
         canvas.add(imgInstance);
+        imgInstance.lockRotation=true;
     }
-
         canvasAnimation(canvas)
 }
 
 function canvasAnimation(canvas){
-    canvas.on('mouse:down', function(opt) {
-        var evt = opt.e;
-        if (evt.altKey === true) {
-            this.isDragging = true;
-            this.selection = false;
-            this.lastPosX = evt.clientX;
-            this.lastPosY = evt.clientY;
+    // canvas.on('mouse:down', function(opt) {
+    //     var evt = opt.e;
+    //     if (evt.altKey === true) {
+    //         this.isDragging = true;
+    //         this.selection = false;
+    //         this.lastPosX = evt.clientX;
+    //         this.lastPosY = evt.clientY;
+    //
+    //     }
+    // });
+    canvas.on('object:moving', function(e) {
+
+        var object = e.target;
+
+        object.setCoords();
+
+
+
+        var boundingRect = object.getBoundingRect();
+        var zoom = canvas.getZoom();
+        var viewportMatrix = canvas.viewportTransform;
+
+        //there is a bug in fabric that causes bounding rects to not be transformed by viewport matrix
+        //this code should compensate for the bug for now
+
+
+        //TODO: viewport veranderen zodat hij niet overal heen schiet als je img aanklikt.
+        this.viewportTransform[4] += e.clientX - this.lastPosX;
+        this.viewportTransform[5] += e.clientY - this.lastPosY;
+        this.requestRenderAll();
+        this.lastPosX = e.clientX;
+        this.lastPosY = e.clientY;
+        boundingRect.top = (boundingRect.top - viewportMatrix[5]) / zoom;
+        boundingRect.left = (boundingRect.left - viewportMatrix[4]) / zoom;
+        boundingRect.width /= zoom;
+        boundingRect.height /= zoom;
+
+
+        // if object is too big ignore
+
+        if (object.currentHeight * zoom > object.canvas.height * zoom || object.currentWidth * zoom > object.canvas.width * zoom) {
+            return;
         }
-    });
-    canvas.on('mouse:move', function(opt) {
-        if (this.isDragging) {
-            var e = opt.e;
-            this.viewportTransform[4] += e.clientX - this.lastPosX;
-            this.viewportTransform[5] += e.clientY - this.lastPosY;
-            this.requestRenderAll();
-            this.lastPosX = e.clientX;
-            this.lastPosY = e.clientY;
+
+        var canvasHeight = object.canvas.height / zoom,
+            canvasWidth = object.canvas.width / zoom,
+            rTop = boundingRect.top + boundingRect.height,
+            rLeft = boundingRect.left + boundingRect.width;
+
+        // top-left  corner
+        if (rTop < canvasHeight || rLeft < canvasWidth) {
+            object.top = Math.max(object.top, canvasHeight - boundingRect.height);
+            object.left = Math.max(object.left, canvasWidth - boundingRect.width);
         }
+
+
+        // bot-right corner
+        if (boundingRect.top + boundingRect.height > object.canvas.height || boundingRect.left + boundingRect.width > object.canvas.width) {
+
+            object.top = Math.min(object.top, object.canvas.height - boundingRect.height + object.top - boundingRect.top);
+            object.left = Math.min(object.left, object.canvas.width - boundingRect.width + object.left - boundingRect.left);
+        }
+
     });
     canvas.on('mouse:up', function() {
         this.isDragging = false;
@@ -53,7 +98,7 @@ function canvasAnimation(canvas){
         var delta = -opt.e.deltaY;
         var pointer = canvas.getPointer(opt.e);
         var zoom = canvas.getZoom();
-        zoom = zoom + delta/500;
+        zoom = zoom + delta/200;
         if (zoom > 15) zoom = 15;
         if (zoom < 0.075) zoom = 0.075;
         canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
@@ -61,4 +106,17 @@ function canvasAnimation(canvas){
         opt.e.stopPropagation();
     });
 
+
+
+
+    // canvas.on('mouse:move', function(opt) {
+    //     if (this.isDragging) {
+    //         var e = opt.e;
+    //         this.viewportTransform[4] += e.clientX - this.lastPosX;
+    //         this.viewportTransform[5] += e.clientY - this.lastPosY;
+    //         this.requestRenderAll();
+    //         this.lastPosX = e.clientX;
+    //         this.lastPosY = e.clientY;
+    //
+    //     }});
 }
